b26043e5f393bcad00a40f91f8998c722548ef4f
Heark <antwauntune25@wcpsstudent.org>
2016-05-18 13:43:44 -0400

    Update gavin.js

--- gavin.js
+++ gavin.js
@@ -4,15 +4,19 @@ var Gavin = require('cylon');
 Gavin.robot({
   connections: {
     keyboard: { adaptor: 'keyboard' }
+    raspi: { adaptor: 'raspi' }
   },
 
   devices: {
     keyboard: { driver: 'keyboard' }
+    led: { driver: 'led', pin: 11 }
   },
 
   work: function(my) {
     my.keyboard.on('a', function(key) {
       console.log("A PRESSED!");
     });
+    every((1).second(), my.led.toggle) // Flash LED when Raspberry Pi is on
+    
   }
 }).start();
