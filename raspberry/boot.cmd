ac0be2eb2d90ad68ddf6dbed629a7918a7b6feeb
Heark <antwauntune25@wcpsstudent.org>
2016-05-19 08:02:57 -0400

    Update boot.cmd

--- raspberry/boot.cmd
+++ raspberry/boot.cmd
@@ -2,9 +2,9 @@ echo "Welcome to Gavin"
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
 sudo dpkg -i node_latest_armhf.deb
 
-su pi -c 'node /home/pi/gavin.js < /dev/null &'
+su pi -c 'node /home/pi/RR1.js < /dev/null &'
 
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb 
 sudo dpkg -i node_latest_armhf.deb 
-touch /home/pi/gavin.js 
-su pi -c 'node /home/pi/gavin.js < /dev/null &'
+touch /home/pi/RR1.js 
+su pi -c 'node /home/pi/RR1.js < /dev/null &'
